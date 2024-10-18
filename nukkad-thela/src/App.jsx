a87670f358959dcfce3bcdaf34d195432532dc22
amanurmaliya <amanurmaliya609@gmail.com>
2024-10-18 11:30:46 +0530

    Merging Aman For the integration of the shadcn ui , frontend and backend starter files

--- nukkad-thela/src/App.jsx
+++ nukkad-thela/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <div className="">Radhe Radhe</div>{" "}
+      <Button variant="destructive">This is the button from shadcn ui</Button>
     </>
   );
 }
